9ab59550a69d19d07adf80d4641f94068e1c10a7
nilo-byte <80394658+nilo-byte@users.noreply.github.com>
2021-03-10 12:09:21 +0100

    S3 Bucket - Origin checkup
    Compatible with S3 Bucket instead of Google Cloud
    If asset is not found in Bucket, try to pull asset from origin server.

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
 })
 
-const BUCKET_NAME = ''
-const BUCKET_URL = `http://storage.googleapis.com/${BUCKET_NAME}`
+const ORIGIN_URL = `https://www.example.com`
+const BUCKET_URL = `https://examplebucket.s3.amazonaws.com`
 
 async function serveAsset(event) {
   const url = new URL(event.request.url)
   const cache = caches.default
   let response = await cache.match(event.request)
-
   if (!response) {
     response = await fetch(`${BUCKET_URL}${url.pathname}`)
-    const headers = { 'cache-control': 'public, max-age=14400' }
+    const headers = { 'cache-control': 'public, max-age=2592000' }
     response = new Response(response.body, { ...response, headers })
     event.waitUntil(cache.put(event.request, response.clone()))
   }
+  return response
+}
+
+async function serveAssetOrigin(event) {
+  const url = new URL(event.request.url)
+  const cache = caches.default
+  response = await fetch(`${ORIGIN_URL}${url.pathname}`)
+  const headers = { 'cache-control': 'public, max-age=2592000' }
+  response = new Response(response.body, { ...response, headers })
+  event.waitUntil(cache.put(event.request, response.clone()))
   return response
 }
 
@@ -23,10 +32,14 @@ async function handleRequest(event) {
   if (event.request.method === 'GET') {
     let response = await serveAsset(event)
     if (response.status > 399) {
-      response = new Response(response.statusText, { status: response.status })
+      let response = await serveAssetOrigin(event)
+      if (response.status > 399) {
+        response = new Response(response.statusText, { status: response.status })
+      }
+      return response
+    } else {
+      return new Response('Method not allowed', { status: 405 })
+    }
     }
     return response
-  } else {
-    return new Response('Method not allowed', { status: 405 })
-  }
 }
