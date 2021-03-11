25b62e33a53bb74340b5baa2a4023b3d442bf31b
nilo-byte <80394658+nilo-byte@users.noreply.github.com>
2021-03-11 19:18:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ addEventListener('fetch', event => {
 })
 
 const ORIGIN_URL = `https://www.example.com`
-const BUCKET_URL = `https://examplebucket.s3.amazonaws.com`
+const BUCKET_URL = `https://example.s3.amazonaws.com`
 
 async function serveAsset(event) {
   const url = new URL(event.request.url)
   const cache = caches.default
   let response = await cache.match(event.request)
+
   if (!response) {
     response = await fetch(`${BUCKET_URL}${url.pathname}`)
     const headers = { 'cache-control': 'public, max-age=2592000' }
@@ -18,28 +19,22 @@ async function serveAsset(event) {
   return response
 }
 
-async function serveAssetOrigin(event) {
-  const url = new URL(event.request.url)
-  const cache = caches.default
-  response = await fetch(`${ORIGIN_URL}${url.pathname}`)
-  const headers = { 'cache-control': 'public, max-age=2592000' }
-  response = new Response(response.body, { ...response, headers })
-  event.waitUntil(cache.put(event.request, response.clone()))
-  return response
-}
-
 async function handleRequest(event) {
   if (event.request.method === 'GET') {
     let response = await serveAsset(event)
     if (response.status > 399) {
-      let response = await serveAssetOrigin(event)
+      const url = new URL(event.request.url)
+      const cache = caches.default
+      response = await fetch(`${ORIGIN_URL}${url.pathname}`)
+      const headers = { 'cache-control': 'public, max-age=2592000' }
+      response = new Response(response.body, { ...response, headers })
+      event.waitUntil(cache.put(event.request, response.clone()))
       if (response.status > 399) {
         response = new Response(response.statusText, { status: response.status })
       }
-      return response
-    } else {
-      return new Response('Method not allowed', { status: 405 })
-    }
     }
     return response
+  } else {
+    return new Response('Method not allowed', { status: 405 })
+  }
 }
